1d5fdd0aad6d96728f58e7aaea0bb5d02af7b466
Shrey Goel <45508538+shreygoel7@users.noreply.github.com>
2021-11-27 17:49:53 +0530

    Added Database and updated settings.

--- Pinocchio/Pinocchio/settings.py
+++ Pinocchio/Pinocchio/settings.py
@@ -81,26 +81,13 @@ WSGI_APPLICATION = 'Pinocchio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "pinocchiodb",
-        'USER': "shrey",
-        'PASSWORD': "shrey12345",
-        'HOST': "localhost",
-        'PORT': "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
